24c923f8cc5dd0333d9a53f24ac96133e1e640f0
PrzemyslawPolrolniczak <przemekp96@gmail.com>
2020-10-05 22:14:13 +0200

    add: tile component

--- lib/widgets/tile.dart
+++ lib/widgets/tile.dart
@@ -1,90 +1,106 @@
 import 'package:flutter/material.dart';
 
 import '../consts.dart';
+import '../routes/reservationDetails.dart';
 
 class Tile extends StatelessWidget {
-  Tile({@required this.status});
+  Tile(
+      {@required this.status,
+      @required this.author,
+      @required this.dateRange,
+      @required this.peopleSigned});
 
   final Status status;
+  final String author;
+  final String dateRange;
+  final int peopleSigned;
 
-  get getStatusColor {
+  get getStatus {
     switch (status) {
       case Status.open:
         {
-          return Colors.green;
+          return {"color": Colors.green, "text": 'otwarty'};
         }
       case Status.ongoing:
         {
-          return Colors.amber;
+          return {"color": Colors.amber, "text": 'w trakcie'};
         }
       case Status.private:
         {
-          return Colors.redAccent;
+          return {"color": Colors.redAccent, "text": 'prywatny'};
         }
       case Status.past:
         {
-          return Colors.grey;
+          return {"color": Colors.grey, "text": 'przeszły'};
         }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 85,
-      margin: EdgeInsets.only(bottom: 8),
-      child: Card(
-        elevation: 3,
-        child: Row(
-          children: [
-            Flexible(
-              flex: 1,
-              fit: FlexFit.loose,
-              child: Container(
-                height: double.infinity,
-                width: 16,
-                color: getStatusColor,
-              ),
-            ),
-            Flexible(
-              flex: 30,
-              child: Container(
-                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 8),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          margin: EdgeInsets.only(bottom: 8),
-                          child: Text(
-                            '24.08 - 02.09',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              "Status: w trakcie,  twórca: Rysiek",
-                              style: TextStyle(fontSize: 12),
-                            )
-                          ],
-                        ),
-                      ])),
-            ),
-            Flexible(
-              flex: 10,
-              child: Container(
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ReservationDetails()),
+        );
+      },
+      child: Container(
+        height: 85,
+        margin: EdgeInsets.only(bottom: 8),
+        child: Card(
+          elevation: 3,
+          child: Row(
+            children: [
+              Flexible(
+                flex: 1,
+                fit: FlexFit.loose,
+                child: Container(
                   height: double.infinity,
-                  child: Text(
-                    '5 os.',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    border: Border(
-                        left: BorderSide(color: Colors.grey[300], width: 1)),
-                  )),
-            )
-          ],
+                  width: 16,
+                  color: getStatus["color"],
+                ),
+              ),
+              Flexible(
+                flex: 30,
+                child: Container(
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(bottom: 8),
+                            child: Text(
+                              dateRange,
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "Status: ${getStatus['text']},  twórca: $author",
+                                style: TextStyle(fontSize: 12),
+                              )
+                            ],
+                          ),
+                        ])),
+              ),
+              Flexible(
+                flex: 10,
+                child: Container(
+                    height: double.infinity,
+                    child: Text(
+                      '$peopleSigned os.',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      border: Border(
+                          left: BorderSide(color: Colors.grey[300], width: 1)),
+                    )),
+              )
+            ],
+          ),
         ),
       ),
     );
